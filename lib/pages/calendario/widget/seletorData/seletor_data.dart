abb6825e415318cd5f5bf10a9f4d60e57e0c598c
Matheus Tonelli <mat.tonelliandrade@gmail.com>
2020-12-22 10:11:35 -0400

    adicionando padding ao seletor de datas

--- lib/pages/calendario/widget/seletorData/seletor_data.dart
+++ lib/pages/calendario/widget/seletorData/seletor_data.dart
@@ -15,28 +15,31 @@ class _SeletorDataState extends State<SeletorData> {
   Widget build(BuildContext context) {
     var listaRevisoesState = Provider.of<ListaRevisoesModel>(context);
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            listaRevisoesState.dataSelecionada = listaRevisoesState.dataSelecionada.subtract(Duration(days: 1));
-          },
-          child: Icon(Icons.arrow_back_outlined),
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            listaRevisoesState.dataSelecionada = await escolherDataDialog(context, listaRevisoesState.dataSelecionada);
-          },
-          child: Text(botaoDataToString(listaRevisoesState.dataSelecionada)),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            listaRevisoesState.dataSelecionada = listaRevisoesState.dataSelecionada.add(Duration(days: 1));
-          },
-          child: Icon(Icons.arrow_forward_outlined),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              listaRevisoesState.dataSelecionada = listaRevisoesState.dataSelecionada.subtract(Duration(days: 1));
+            },
+            child: Icon(Icons.arrow_back_outlined),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              listaRevisoesState.dataSelecionada = await escolherDataDialog(context, listaRevisoesState.dataSelecionada);
+            },
+            child: Text(botaoDataToString(listaRevisoesState.dataSelecionada)),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              listaRevisoesState.dataSelecionada = listaRevisoesState.dataSelecionada.add(Duration(days: 1));
+            },
+            child: Icon(Icons.arrow_forward_outlined),
+          ),
+        ],
+      ),
     );
   }
 
