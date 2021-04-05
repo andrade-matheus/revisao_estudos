a7193b8d3ba295781d933c3c5938bbe19c27c40d
Matheus Tonelli <mat.tonelliandrade@gmail.com>
2021-04-05 16:55:26 -0400

    Criando as funcionalidades de backup

--- lib/backup/backup.dart
+++ lib/backup/backup.dart
@@ -1,37 +1,73 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:revisao_estudos/controllers/database_controller.dart';
 import 'package:revisao_estudos/database/database_creator.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:convert' as convert;
 
 class Backup {
-  
+
   List<String> tables = [
-    DatabaseController.revisaoTable,
-    DatabaseController.logRevisaoTable,
     DatabaseController.disciplinaTable,
     DatabaseController.frequenciaTable,
+    DatabaseController.revisaoTable,
+    DatabaseController.logRevisaoTable,
   ];
 
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
+  // CRIAÇÃO DO BACKUP
 
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    print(path);
-    return File('$path/backup.json');
+  Future<bool> gerarBackup() async {
+    print('GERANDO BACKUP');
+
+    List data = [];
+
+    List<Map<String, dynamic>> listMaps = [];
+
+    for (var i = 0; i < tables.length; i++) {
+      listMaps = await db.query(tables[i]);
+      data.add(listMaps);
+    }
+
+    List backups = [tables, data];
+    String json = convert.jsonEncode(backups);
+    writeToBackupFile(json);
+
+    return true;
   }
 
   Future<File> writeToBackupFile(String json) async {
-    final file = await _localFile;
-
+    String path = await FilePicker.platform.getDirectoryPath();
+    final file = File('$path/revisao_backup.txt');
     // Write the file.
     return file.writeAsString(json);
+  }
+
+  // RESTAURANDO O BACKUP
+  Future<void> restoreBackup() async {
+    FilePickerResult result = await FilePicker.platform.pickFiles();
+    if(result != null) {
+      String path = result.files.single.path;
+      String backup = await readFile(path);
+
+      await clearAllTables();
+
+      Batch batch = db.batch();
+      List json = convert.jsonDecode(backup);
+
+      for (var i = 0; i < json[0].length; i++) {
+        for (var k = 0; k < json[1][i].length; k++) {
+          batch.insert(json[0][i], json[1][i][k]);
+        }
+      }
+
+      await batch.commit(continueOnError: false, noResult: true);
+      print('BACKUP RESTAURADO COM SUCESSO!!');
+    }
+    else{
+      print('NÃO FOI POSSIVEL RESTAURAR O BACKUP');
+    }
   }
 
   Future<String> readFile(String path) async {
@@ -65,45 +101,5 @@ class Backup {
         print('------ DELETE $table');
       }
     } catch (e) {}
-  }
-
-  Future<String> generateBackup() async {
-    print('GENERATE BACKUP');
-
-    List data = [];
-
-    List<Map<String, dynamic>> listMaps = [];
-
-    for (var i = 0; i < tables.length; i++) {
-      listMaps = await db.query(tables[i]);
-
-      data.add(listMaps);
-    }
-
-    List backups = [tables, data];
-
-    String json = convert.jsonEncode(backups);
-
-    writeToBackupFile(json);
-
-    return json;
-  }
-
-  Future<void> restoreBackup(String path) async {
-
-    String backup = await readFile(path);
-    print(backup);
-    // Batch batch = db.batch();
-    //
-    // List json = convert.jsonDecode(backup);
-    //
-    // for (var i = 0; i < json[0].length; i++) {
-    //   for (var k = 0; k < json[1][i].length; k++) {
-    //     batch.insert(json[0][i], json[1][i][k]);
-    //   }
-    // }
-    //
-    // await batch.commit(continueOnError: false, noResult: true);
-    print('RESTORE BACKUP');
   }
 }
