6deac5c3fbfea2ff76730f5d3a8a45f10df7fe0c
Matheus Tonelli <mat.tonelliandrade@gmail.com>
2021-11-19 09:37:22 -0400

    Renomeando arquivo

--- lib/screens/calendario/calendario_revisoes_widget/lista_revisoes_widget/lista_revisoes.dart
+++ lib/screens/calendario/calendario_revisoes_widget/lista_revisoes_widget/lista_revisoes.dart
@@ -3,26 +3,26 @@ import 'package:provider/provider.dart';
 import 'package:revisao_estudos/models/entity/disciplina.dart';
 import 'package:revisao_estudos/models/entity/revisao.dart';
 import 'package:revisao_estudos/models/provider/data_selecionada.dart';
+import 'package:revisao_estudos/screens/calendario/calendario_revisoes_widget/tiles_widgets/log_tile.dart';
 import 'package:revisao_estudos/screens/calendario/calendario_revisoes_widget/tiles_widgets/revisao_tile.dart';
 import 'package:revisao_estudos/services/repositories/repository_revisao.dart';
+import 'package:revisao_estudos/utils/date/date_helper.dart';
 
-class ListaRevisoesDisciplina extends StatelessWidget {
+class ListaRevisoes extends StatelessWidget {
   final Disciplina disciplina;
 
-  const ListaRevisoesDisciplina({
+  const ListaRevisoes({
     Key key,
     @required this.disciplina,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    DateTime data = context.read<DataSelecionada>().dataSelecionada;
     RepositoryRevisao repositoryRevisao = RepositoryRevisao();
     List<Revisao> revisoes = [];
     return FutureBuilder(
-      future: repositoryRevisao.obterTodosPorDisciplinaPorData(
-        disciplina,
-        context.read<DataSelecionada>().dataSelecionada,
-      ),
+      future: repositoryRevisao.obterParaCaledario(disciplina, data),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           revisoes = snapshot.data;
@@ -30,7 +30,13 @@ class ListaRevisoesDisciplina extends StatelessWidget {
             physics: ClampingScrollPhysics(),
             shrinkWrap: true,
             itemCount: revisoes.length,
-            itemBuilder: (context, index) => RevisaoTile(revisao: revisoes[index]),
+            itemBuilder: (context, index) => DateHelper.isLog(data)
+                ? LogTile(
+                    revisao: revisoes[index],
+                  )
+                : RevisaoTile(
+                    revisao: revisoes[index],
+                  ),
           );
         } else {
           return CircularProgressIndicator();
