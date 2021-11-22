f6b8463eeb80563a8018983313d888db0db8b250
Matheus Tonelli <mat.tonelliandrade@gmail.com>
2021-11-22 13:43:05 -0400

    Melhorando queries do repositório de disciplinas.

--- lib/services/repositories/repository_disciplina.dart
+++ lib/services/repositories/repository_disciplina.dart
@@ -1,8 +1,6 @@
 import 'package:revisao_estudos/models/entity/disciplina.dart';
 import 'package:revisao_estudos/services/database/database_config.dart';
 import 'package:revisao_estudos/services/repositories/repository_common.dart';
-import 'package:revisao_estudos/services/repositories/repository_log_revisao.dart';
-import 'package:revisao_estudos/services/repositories/repository_revisao.dart';
 import 'package:revisao_estudos/utils/date/date_helper.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -21,36 +19,41 @@ class RepositoryDisciplina extends RepositoryCommon<Disciplina> {
     return _database;
   }
 
-  Future<List<Disciplina>> obterTodosComRevisoesPorData(DateTime data) async {
-    List<Disciplina> disciplinas = [];
-
+  Future<List<Disciplina>> obterTodasComRevisoesPorData(DateTime data) async {
     if (data.isBefore(DateHelper.hoje)) {
-      var repository = RepositoryLogRevisao();
-      var logRevisoes = await repository.obterTodosPorData(data);
-      for (var logRev in logRevisoes) {
-        if (!disciplinas.contains(logRev.revisao.disciplina)) {
-          disciplinas.add(logRev.revisao.disciplina);
-        }
-      }
+      return await obterTodasComLogRevisoesPorData(data);
     } else if (data.isAfter(DateHelper.amanha) || data.isAtSameMomentAs(DateHelper.amanha)){
-      disciplinas = await _obterTodosComRevisoesPorData(data);
+      return await _obterTodasComRevisoesPorData(data);
     } else {
-      disciplinas = await obterTodasComRevisoesParaHoje();
+      return await obterTodasComRevisoesParaHoje();
     }
+  }
+
+  // Retorna as disciplinas que tem Logs de Revisão para esse determinado dia.
+  Future<List<Disciplina>> obterTodasComLogRevisoesPorData(DateTime data) async {
+    List<Disciplina> disciplinas = [];
+    String query = """SELECT DISTINCT disciplina.id, disciplina.nome
+                      FROM logRevisao 
+                      INNER JOIN revisao ON logRevisao.idRevisao = revisao.id
+                      INNER JOIN disciplina ON revisao.idDisciplina = disciplina.id
+                      WHERE date(logRevisao.dataRevisao) = date(?);
+                      """;
+    List<Object> arguments = [DateHelper.formatarParaSql(data)];
+    disciplinas = await obterPorRawQuery(query, arguments);
 
     return disciplinas;
   }
 
-  // Retorna as disciplinas que tem revisões para esse determinado dia e NÃO incluí as revisões atrasadas.
-  Future<List<Disciplina>> _obterTodosComRevisoesPorData(DateTime data) async {
-    List<Disciplina> disciplinas = [];
 
+  // Retorna as disciplinas que tem revisões para esse determinado dia e NÃO incluí as revisões atrasadas.
+  Future<List<Disciplina>> _obterTodasComRevisoesPorData(DateTime data) async {
+    List<Disciplina> disciplinas = [];
     String query = """SELECT DISTINCT disciplina.id as id, disciplina.nome as nome
                       FROM disciplina INNER JOIN revisao ON disciplina.id = revisao.idDisciplina
-                      WHERE date(revisao.proxRevisao) = date(?);""";
+                      WHERE date(revisao.proxRevisao) = date(?);
+                      """;
 
     List<Object> arguments = [DateHelper.formatarParaSql(data)];
-
     disciplinas = await obterPorRawQuery(query, arguments);
 
     return disciplinas;
@@ -59,12 +62,10 @@ class RepositoryDisciplina extends RepositoryCommon<Disciplina> {
   // Retorna disciplinas que tem revisões para hoje incluindo as atrasadas.
   Future<List<Disciplina>> obterTodasComRevisoesParaHoje() async {
     List<Disciplina> disciplinas = [];
-
     String query = """SELECT DISTINCT disciplina.id as id, disciplina.nome as nome
                       FROM disciplina INNER JOIN revisao ON disciplina.id = revisao.idDisciplina
                       WHERE date(revisao.proxRevisao) < date(?);""";
     List<Object> arguments = [DateHelper.formatarParaSql(DateHelper.hoje)];
-
     disciplinas = await obterPorRawQuery(query, arguments);
 
     return disciplinas;
