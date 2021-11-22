bd552b04fa5f162f636299a3cb56432bbf4279be
Matheus Tonelli <mat.tonelliandrade@gmail.com>
2021-11-22 14:28:24 -0400

    Otimizando queries do repositório de revisões.

--- lib/services/repositories/repository_revisao.dart
+++ lib/services/repositories/repository_revisao.dart
@@ -30,42 +30,53 @@ class RepositoryRevisao extends RepositoryCommon<Revisao> {
     return await fromMapList(resultado);
   }
 
-  Future<List<Revisao>> obterEmLogPorDisciplinaPorData(Disciplina disciplina, DateTime data) async {
-    final bd = await database;
-    var dataSql = DateHelper.formatarParaSql(data);
-    String sql = """ 
-      SELECT revisao.id, idDisciplina, idFrequencia, nome, dataCadastro, proxRevisao, vezesRevisadas, isArchived
-      FROM logRevisao
-      INNER JOIN revisao on revisao.id = logRevisao.idRevisao
-      WHERE date(proxRevisao) == date($dataSql) AND idDisciplina == ${disciplina.id}
-    """;
-    var resultado = await bd.rawQuery(sql);
-    return await fromMapList(resultado);
-  }
-
-  // Todas revisões de uma disciplina para essa data.
-  Future<List<Revisao>> obterPorDisciplinaPorData(Disciplina disciplina, DateTime data) async {
-    List<Revisao> revisoes = await obterPorDisciplina(disciplina);
-    revisoes.removeWhere((revisao) => !DateHelper.isSameDay(revisao.proxRevisao, data));
-
-    return revisoes;
-  }
-
-  // Todas revisões de uma disciplina para essa data incluindo atrasadas.
-  Future<List<Revisao>> obterPorDisciplinaPorDataComAtrasadas(Disciplina disciplina, DateTime data) async {
-    List<Revisao> revisoes = await obterPorDisciplina(disciplina);
-    revisoes.removeWhere((revisao) => !revisao.proxRevisao.isBefore(data.add(Duration(days: 1))));
-
-    return revisoes;
-  }
-
-  Future<List<Revisao>> obterParaCaledario(Disciplina disciplina, DateTime data) async {
+  Future<List<Revisao>> obterParaCaledario(
+      Disciplina disciplina, DateTime data) async {
     if (data.isBefore(DateHelper.hoje())) {
       return await obterEmLogPorDisciplinaPorData(disciplina, data);
-    } else if(data.isBefore(DateHelper.amanha())) {
+    } else if (data.isBefore(DateHelper.amanha())) {
       return await obterPorDisciplinaPorDataComAtrasadas(disciplina, data);
     } else {
       return await obterPorDisciplinaPorData(disciplina, data);
     }
+  }
+
+  // Retorna as revisões que foram concluídas de uma disciplina em determinada data.
+  Future<List<Revisao>> obterEmLogPorDisciplinaPorData(
+      Disciplina disciplina, DateTime data) async {
+    String query = """SELECT revisao.id, 
+                              idDisciplina, 
+                              idFrequencia, 
+                              nome, 
+                              dataCadastro, 
+                              proxRevisao, 
+                              vezesRevisadas, 
+                              isArchived
+                      FROM logRevisao
+                      INNER JOIN revisao on revisao.id = logRevisao.idRevisao
+                      WHERE date(dataRevisao) = date(?) AND idDisciplina = ?;""";
+
+    List<Object> arguments = [DateHelper.formatarParaSql(data), disciplina.id];
+    return await obterPorRawQuery(query, arguments);
+  }
+
+  // Retorna as revisões de uma disciplina em determinada data.
+  Future<List<Revisao>> obterPorDisciplinaPorData(Disciplina disciplina, DateTime data) async {
+    String query = """SELECT *
+                      FROM revisao
+                      WHERE date(proxRevisao) = date(?) AND idDisciplina == ?;""";
+
+    List<Object> arguments = [DateHelper.formatarParaSql(data), disciplina.id];
+    return await obterPorRawQuery(query, arguments);
+  }
+
+  // Retorna as revisões de uma disciplina para determinada data, mais as revisões atrasadas (de datas anteriores).
+  Future<List<Revisao>> obterPorDisciplinaPorDataComAtrasadas(Disciplina disciplina, DateTime data) async {
+    String query = """SELECT *
+                      FROM revisao
+                      WHERE date(proxRevisao) <= date(?) AND idDisciplina == ?;""";
+
+    List<Object> arguments = [DateHelper.formatarParaSql(data), disciplina.id];
+    return await obterPorRawQuery(query, arguments);
   }
 }
