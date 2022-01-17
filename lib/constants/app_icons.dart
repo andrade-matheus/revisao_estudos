567bafe7bd94b9d5200c5a8f1b1685b7a7038c79
Matheus Tonelli <mat.tonelliandrade@gmail.com>
2022-01-17 08:45:58 -0400

    Create app_icons.dart

---
+++ lib/constants/app_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const IconData calendario = Icons.calendar_today;
+  static const IconData revisoes = Icons.bookmark_border_outlined;
+  static const IconData disciplinas = Icons.book_outlined;
+  static const IconData frequencias = Icons.repeat;
+  static const IconData configuracoes = Icons.settings_outlined;
+}
