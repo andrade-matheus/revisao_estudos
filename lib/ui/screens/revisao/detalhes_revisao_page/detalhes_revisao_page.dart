724bad31683d8e2dea61710232d615ec1e7418ac
Matheus Tonelli <mat.tonelliandrade@gmail.com>
2022-01-21 10:34:00 -0400

    Corrigindo pagina de detalhes de revisão.

--- lib/ui/screens/revisao/detalhes_revisao_page/detalhes_revisao_page.dart
+++ lib/ui/screens/revisao/detalhes_revisao_page/detalhes_revisao_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:revisao_estudos/models/entity/disciplina.dart';
+import 'package:revisao_estudos/models/entity/frequencia.dart';
 import 'package:revisao_estudos/models/entity/revisao.dart';
+import 'package:revisao_estudos/services/repositories/repository_disciplina.dart';
+import 'package:revisao_estudos/services/repositories/repository_frequencia.dart';
 import 'package:revisao_estudos/ui/screens/revisao/detalhes_revisao_page/detalhes_card_widget/detalhes_card.dart';
 
 class DetalhesRevisaoPage extends StatefulWidget {
@@ -16,35 +20,46 @@ class DetalhesRevisaoPage extends StatefulWidget {
 }
 
 class _DetalhesRevisaoPageState extends State<DetalhesRevisaoPage> {
+  late Disciplina _disciplina;
+  late Frequencia _frequencia;
+
+  @override
+  Future<void> initState() async {
+    RepositoryDisciplina repositoryDisciplina = RepositoryDisciplina();
+    RepositoryFrequencia repositoryFrequencia = RepositoryFrequencia();
+    _disciplina = (await repositoryDisciplina.obterPorId(widget.revisao.disciplinaId))!;
+    _frequencia = (await repositoryFrequencia.obterPorId(widget.revisao.frequenciaId))!;
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         DetalhesCard(
           title: "Disciplina: ",
-          text: this.widget.revisao.disciplina?.nome ?? '',
+          text: _disciplina.nome,
         ),
         DetalhesCard(
           title: "Nome da revisão: ",
-          text: this.widget.revisao.nome,
+          text: widget.revisao.nome,
         ),
         DetalhesCard(
           title: "Frequência: ",
-          text: this.widget.revisao.frequencia?.frequencia ?? '',
+          text: _frequencia.frequencia,
         ),
         DetalhesCard(
           title: "Quantidade de revisões concluídas: ",
-          text: this.widget.revisao.vezesRevisadas.toString(),
+          text: widget.revisao.vezesRevisadas.toString(),
         ),
         DetalhesCard(
           title: "Data do cadastro: ",
-          text: DateFormat('dd / MM / yyyy')
-              .format(this.widget.revisao.dataCadastro),
+          text: DateFormat('dd / MM / yyyy').format(widget.revisao.dataCadastro),
         ),
         DetalhesCard(
           title: "Data da próxima revisão: ",
-          text: DateFormat('dd / MM / yyyy')
-              .format(this.widget.revisao.proxRevisao),
+          text: DateFormat('dd / MM / yyyy').format(widget.revisao.proxRevisao),
         ),
       ],
     );
