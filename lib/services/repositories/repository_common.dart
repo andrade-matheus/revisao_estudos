90ad10ae009dd9b01e018d2cd40205f4c9b9fafb
Matheus Tonelli <mat.tonelliandrade@gmail.com>
2022-03-31 11:32:06 -0400

    Refatorando repository common.

--- lib/services/repositories/repository_common.dart
+++ lib/services/repositories/repository_common.dart
@@ -5,69 +5,72 @@ import 'package:sqflite/sqflite.dart';
 abstract class RepositoryCommon<T extends EntityCommon> {
   static Database? _database;
 
-  String get nomeTabela;
+  String get _tableName;
   Function get fromMap;
-  Future<bool> utilizado(int id);
+  Future<bool> isBeingUsed(int id);
 
   Future<Database?> get database async {
-    if (_database != null) return _database;
+    if (_database != null)
+      return _database;
+
     _database = await DatabaseConfig.initDB();
+
     return _database;
   }
 
-  Future<T> adicionar(T param) async {
-    Map<String, dynamic> map = param.toMap();
+  Future<T> add(T item) async {
+    Map<String, dynamic> map = item.toMap();
     map.remove('id');
 
     final bd = await database;
     await bd?.transaction((txn) async {
-      param.id = await txn.insert(
-        nomeTabela,
+      item.id = await txn.insert(
+        _tableName,
         map,
       );
     });
-    return param;
+    return item;
   }
 
-  Future<T?> obterPorId(int id) async {
+  Future<T?> getById(int id) async {
     final db = await database;
-    var resultado = await db?.query(
-      nomeTabela,
+    var result = await db?.query(
+      _tableName,
       where: 'id = ?',
       whereArgs: [id],
     );
-    return resultado?.isNotEmpty ?? false ? await fromMap(resultado?.first) as T : null;
+    return result?.isNotEmpty ?? false ? await fromMap(result?.first) as T : null;
   }
 
-  Future<List<T>> obterTodos() async {
+  Future<List<T>> getAll() async {
     final bd = await database;
-    var resultado = await bd?.query(nomeTabela);
-    List<T> lista =  await fromMapList(resultado ?? []);
+    var result = await bd?.query(_tableName);
+    List<T> lista =  await fromMapListToItemList(result ?? []);
     return lista;
   }
 
-  Future<List<T>> obterPor({required String where, required List<Object> whereArgs}) async {
+  Future<List<T>> getByQuery({required String where, required List<Object> whereArgs}) async {
     final bd = await database;
-    var resultado = await bd?.query(
-      nomeTabela,
+    var result = await bd?.query(
+      _tableName,
       where: where,
       whereArgs: whereArgs,
     );
-    List<T> lista =  await fromMapList(resultado ?? []);
+    List<T> lista =  await fromMapListToItemList(result ?? []);
     return lista;
   }
 
   Future<List<T>> obterPorRawQuery(String query, List<Object> arguments) async {
     final bd = await database;
-    var resultado = await bd?.rawQuery(query, arguments);
-    List<T> lista =  await fromMapList(resultado ?? []);
+    var result = await bd?.rawQuery(query, arguments);
+    List<T> lista =  await fromMapListToItemList(result ?? []);
     return lista;
   }
 
-  Future<T> atualizar(T param) async {
+  Future<T> update(T param) async {
     final db = await database;
     await db?.update(
-      nomeTabela,
+      _tableName,
       param.toMap(),
       where: 'id = ?',
       whereArgs: [param.id],
@@ -75,24 +78,14 @@ abstract class RepositoryCommon<T extends EntityCommon> {
     return param;
   }
 
-  Future<bool> existe(int id) async {
-    final bd = await database;
-    var resultado = await bd?.query(
-      nomeTabela,
-      where: 'id = ?',
-      whereArgs: [id],
-    );
-    return resultado?.isNotEmpty ?? false;
-  }
-
-  Future<bool> remover(int id, {bool force = false}) async {
-    if(!force && await utilizado(id))
+  Future<bool> delete(int id, {bool forceDelete = false}) async {
+    if(!forceDelete && await isBeingUsed(id))
       return false;
 
     final db = await database;
     await db?.transaction((txn) async {
       await txn.delete(
-        nomeTabela,
+        _tableName,
         where: 'id = ?',
         whereArgs: [id],
       );
@@ -101,50 +94,23 @@ abstract class RepositoryCommon<T extends EntityCommon> {
     return true;
   }
 
-  Future<void> remoterTodos() async {
+  Future<void> deleteAll() async {
     final db = await database;
     await db?.transaction((txn) async {
-      await txn.rawDelete('DELETE FROM $nomeTabela');
+      await txn.rawDelete('DELETE FROM $_tableName');
     });
   }
 
-  Future<List<T>> adicionarVarios(List<T> param) async {
-    final db = await database;
-    await db?.transaction((txn) async {
-      param.forEach((element) async {
-        await txn.insert(nomeTabela, element.toMap());
-      });
-    });
-    param.sort((a,b) => a.compareTo(b));
-    return param;
-  }
-
-  Future<List<T>> atualizarVarios(List<T> param) async {
-    final db = await database;
-    await db?.transaction((txn) async {
-      param.forEach((element) async {
-        await txn.update(
-          nomeTabela,
-          element.toMap(),
-          where: 'id = ?',
-          whereArgs: [element.id],
-        );
-      });
-    });
-    param.sort((a,b) => a.compareTo(b));
-    return param;
-  }
-
-  Future<List<T>> fromMapList(List<Map> param) async {
-    List<T> lista = [];
-    if (param.isNotEmpty) {
-      for (var item in param) {
-        lista.add(await fromMap(item) as T);
+  Future<List<T>> fromMapListToItemList(List<Map> mapList) async {
+    List<T> items = [];
+    if (mapList.isNotEmpty) {
+      for (var map in mapList) {
+        items.add(await fromMap(map) as T);
       }
     } else {
-      lista = new List<T>.from([]);
+      items = new List<T>.from([]);
     }
-    lista.sort((a,b) => a.compareTo(b));
-    return lista;
+    items.sort((a,b) => a.compareTo(b));
+    return items;
   }
 }
